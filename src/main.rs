4de6f29b6dafa19442e693957ceb5fe72d560a12
skep <skep@freq>
2022-12-10 22:35:33 +0100

    feat: add push/pop methods

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::alloc;
 #[derive(Debug)]
 struct DLL {
     root: *mut Node,
+    last: *mut Node,
     layout: alloc::Layout,
 }
 
@@ -17,10 +18,11 @@ impl DLL {
             (*root).l = root;
             (*root).r = root;
         }
-        let dll = DLL { root, layout };
+        let last = root;
+        let dll = DLL { root, last, layout };
         return dll;
     }
-    fn traverse<F>(&mut self, visit: F)
+    fn traverse_ptr<F>(&mut self, visit: F)
     where
         F: Fn(*mut Node),
     {
@@ -39,9 +41,38 @@ impl DLL {
             }
         }
     }
+    fn traverse<F>(&mut self, visit: F)
+    where
+        F: Fn(&Node),
+    {
+        return self.traverse_ptr(|n| unsafe { visit(&(*n)) });
+    }
     fn delete_all(&mut self) {
         let l = self.layout; // needed because 'self' cannot be borrowed twice
-        self.traverse(|n| unsafe { alloc::dealloc(n as *mut u8, l) });
+        self.traverse_ptr(|n| unsafe { alloc::dealloc(n as *mut u8, l) });
+    }
+    fn push(&mut self, c: char) -> () {
+        unsafe {
+            let node = alloc::alloc(self.layout) as *mut Node;
+            (*node).value = c;
+            (*self.last).r = node;
+            (*self.root).l = node;
+            (*node).l = self.last;
+            (*node).r = self.root;
+            self.last = node;
+        }
+    }
+    fn pop(&mut self) -> char {
+        let value: char;
+        unsafe {
+            (*(*self.last).l).r = (*self.last).r;
+            (*(*self.last).r).l = (*self.last).l;
+            value = (*self.last).value;
+            let oldlast = self.last;
+            self.last = (*self.last).l;
+            alloc::dealloc(oldlast as *mut u8, self.layout);
+        }
+        return value;
     }
 }
 impl Drop for DLL {
@@ -50,7 +81,7 @@ impl Drop for DLL {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Node {
     l: *mut Node,
     r: *mut Node,
@@ -64,22 +95,51 @@ impl Node {
             value: c,
         };
     }
-    //fn Delete(&self) {
-    //    self.l.unwrap().r = self.r.unwrap().l;
-    //    self.r.unwrap().l = self.l.unwrap().r;
-    //}
-    //fn Insert(&'a mut self) {
-    //    self.l = Some(self);
-    //    self.r = Some(self);
-    //}
 }
 
 fn main() {
     let n = Node::new('r');
     println!("{:#?}", n);
-    let dll = DLL::new('r');
+    let mut dll = DLL::new('r');
     println!("{:#?}", dll);
-    //let ar = [1i8, 2i8, 3i8];
-    //let ll = DLL::new(&ar);
-    //println!("{:#?}", ar);
+    dll.push('a');
+    dll.push('b');
+    dll.push('c');
+    dll.pop();
+    println!("{:#?}", dll);
+    dll.traverse(|n| println!("{:#?}", n))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::DLL;
+    use std::sync::Mutex;
+
+    #[test]
+    fn simple_list_1node() {
+        let dll = DLL::new('r');
+        assert!(!dll.root.is_null())
+    }
+
+    #[test]
+    fn simple_list_push_nodes() {
+        let mut dll = DLL::new('1');
+        dll.push('2');
+        dll.push('3');
+        let v = Mutex::new(Vec::new());
+        dll.traverse(|n| println!("{:#?}", n));
+        dll.traverse(|n| v.lock().unwrap().push(n.value));
+        let expected: Vec<char> = vec!['1', '2', '3'];
+        let got = v.lock().unwrap().clone();
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn simple_list_pushandpop_nodes() {
+        let mut dll = DLL::new('1');
+        dll.push('2');
+        dll.push('3');
+        assert_eq!(dll.pop(), '3', "first pop");
+        assert_eq!(dll.pop(), '2', "second pop");
+    }
 }
